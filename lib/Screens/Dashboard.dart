37dc257dfb1ebb03c727f4cf301f9f72c2fc78e7
kassem <kassem@oobconsulting.com>
2024-01-06 22:30:06 +0200

    playing with Container and BoxDecoration

--- lib/Screens/Dashboard.dart
+++ lib/Screens/Dashboard.dart
@@ -13,21 +13,33 @@ class Dashboard extends StatelessWidget {
             style: TextStyle(fontSize: 35, color: Colors.green)),
       ),
       body: Column(children: [
+        Container(
+          width: 250,
+          height: 250,
+          margin: const EdgeInsets.only(top: 20),
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            border: Border.all(color: Colors.black26, width: 10),
+            boxShadow: [BoxShadow(color: Colors.black, offset: Offset(0, 0), blurRadius: 12)],
+            image: DecorationImage(image: AssetImage('images/maxresdefault.jpg'), fit: BoxFit.cover)
+          ),
+        ),
         IconButton(
           icon: const Icon(
             Icons.my_library_music,
           ),
           onPressed: () {},
-          color: Colors.orange,
+          color: Colors.orange[700],
           iconSize: 100,
           splashRadius: 500,
           splashColor: Colors.red,
         ),
-        const Image(image: AssetImage("images/maxresdefault.jpg")),
+        //const Image(image: AssetImage("images/maxresdefault.jpg")),
         Center(
             child: Text(
           "Hello from Dashboard, \n here's a random number: ${getRandom()}",
-          style: TextStyle(fontSize: 25, color: Colors.red),
+          style: const TextStyle(fontSize: 25, color: Colors.blueAccent),
           textAlign: TextAlign.center,
         )),
       ]),
